634ee149544d18031bf40ced21cd7ab3a065be36
ilyas48 <ilyas48@yandex.ru>
2023-03-30 22:30:46 +0300

    new main

--- funcs.py
+++ funcs.py
@@ -1,7 +1,13 @@
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
 
 sum = a + b
 
 print("sum:", sum)
 
+aa = int(input("Введите первое число: "))
+bb = int(input("Введите второе число: "))
+
+sum = aa - bb
+
+print("sum:", sum)
